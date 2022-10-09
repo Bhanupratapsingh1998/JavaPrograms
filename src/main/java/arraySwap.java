f4aaa675b729e65d4865a08a9e07e316988238d4
Bhanu <bhanupratapsinghq@gmail.com>
2022-10-09 21:35:47 +0530

    All commit Arrays code

---
+++ src/main/java/arraySwap.java
@@ -0,0 +1,42 @@
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author User
+ */
+public class arraySwap {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 8, 12, 15};
+//        swap(arr, 0, 4);
+//        System.out.println(Arrays.toString(arr));
+        reverseArr(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+//    static void reverseArr(int arr[]) {
+//        int start = 0;
+//        int end = arr.length - 1;
+//        while (start < end) {
+//            swap(arr,start,end);
+//            start++;
+//            end--;
+//        }
+//
+//    }
+    static void reverseArr(int arr[]) {
+        int end = arr.length - 1;
+        for (int start = 0; start < end; start++) {
+            swap(arr, start, end);
+            end--;
+        }
+
+    }
+
+    static void swap(int arr[], int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
---
+++ src/main/java/arraysInput.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ *
+ * @author User
+ */
+public class arraysInput {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println("Array is : " + arr[i]);
+//        }
+//        for (int num : arr) {
+//            System.out.print(num + " "); // here num represents the element of the array
+//        }
+        System.out.print(Arrays.toString(arr));
+    }
+}
