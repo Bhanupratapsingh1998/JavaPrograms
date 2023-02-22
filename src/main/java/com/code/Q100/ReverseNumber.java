e6e7ffc76115120d2f593ba375294c75d47282ec
Bhanu <bhanupratapsinghq@gmail.com>
2023-02-23 02:03:32 +0530

    Prime number

--- src/main/java/com/code/Q100/ReverseNumber.java
+++ src/main/java/com/code/Q100/ReverseNumber.java
@@ -13,10 +13,16 @@ public class ReverseNumber {
         System.out.println("Enter the number to reverse");
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
-        while (n != 0) {
-            reverse = reverse * 10;
-            reverse = reverse + n % 10;
+        
+        //Using while loop
+       /* while (n != 0) {
+            reverse = reverse * 10 + n % 10;
             n = n / 10;
+        }*/
+       
+       //Using for loop
+        for (; n != 0; n = n / 10) {
+            reverse = reverse * 10 + n % 10;
         }
         System.out.println("Reverse of entered number is " + reverse);
     }
---
+++ src/main/java/com/code/Q100/primeNum.java
@@ -0,0 +1,30 @@
+package com.code.Q100;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class primeNum {
+
+    public static void main(String[] args) {
+
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of prime numbers you want");
+        n = in.nextInt();
+        for (int i = 2; i <= n;i++) {
+           int status = 1;
+            for (int j = 2; j <= i/2; j++) {
+                if (i % j == 0) {
+                    status = 0;
+                    break;
+                }
+            }
+            if (status == 1) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
