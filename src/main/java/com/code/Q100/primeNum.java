420506fac3f1af96718bdded12d3cab3f8a212fc
Bhanu <bhanupratapsinghq@gmail.com>
2023-03-26 00:22:17 +0530

    character patterns

--- src/main/java/com/code/Q100/primeNum.java
+++ src/main/java/com/code/Q100/primeNum.java
@@ -17,7 +17,7 @@ public class primeNum {
         for (int i = 2; i <= n;i++) {
            int status = 1;
             for (int j = 2; j <= i/2; j++) {
-                if (i % j == 0) {
+                 if (i % j == 0) {
                     status = 0;
                     break;
                 }
