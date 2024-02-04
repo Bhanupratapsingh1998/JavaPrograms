224720c601bacb7719e2bf538b3320ce51d72f82
BhanuP <bhanu.pratap@hashstudioz.com>
2024-02-04 18:39:36 +0530

    issue fixed.

---
+++ src/main/java/com/code/matrix_arrays/rowcolMatrix.java
@@ -0,0 +1,37 @@
+package com.code.matrix_arrays;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author User
+ */
+public class rowcolMatrix {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {28, 29, 37, 49},
+            {33, 34, 38, 50}};
+        System.out.println("Element is : "+Arrays.toString(search(arr, 91)));
+    }
+
+    static int[] search(int matrix[][], int target) {
+        int row = 0;
+        int col = matrix.length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return new int[]{row, col};
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+}
